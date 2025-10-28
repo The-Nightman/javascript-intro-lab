c27afa7b8179cd9b4625a7f39af2c0e9027bf3f7
The-Nightman <keenan.brant@yahoo.co.uk>
2025-10-28 10:14:14 +0000

    add imgArr and rotateImg function to index.js with click event for myImages element

--- js/index.js
+++ js/index.js
@@ -11,4 +11,23 @@
   document.querySelector(".reset").addEventListener("click", () => {
     document.body.removeAttribute("class");
   });
+
+  const imgArr = [
+    "images/view1.jpg",
+    "images/view2.jpg",
+    "images/view3.jpg",
+    "images/view4.jpg",
+    "images/view5.jpg",
+    "images/view6.jpg",
+  ];
+
+  const rotateImg = (() => {
+    const imgIndex = imgArr.indexOf(document.querySelector("#myImages").getAttribute("src"));
+    
+    return imgArr[(imgIndex === imgArr.length - 1) ? 0 : imgIndex + 1];
+  });   
+
+  document.querySelector("#myImages").addEventListener("click", () => {
+    document.querySelector("#myImages").setAttribute("src", rotateImg());
+  });
 })();
